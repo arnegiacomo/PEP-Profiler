e449c423e296a5dca304271d41550740a556f656
Arne Munthe-Kaas <45269747+h593267@users.noreply.github.com>
2022-08-28 21:41:48 +0200

    .

--- src/main/java/no/arnemunthekaas/Main.java
+++ src/main/java/no/arnemunthekaas/Main.java
@@ -1,4 +1,6 @@
-package no.arnemunthekaas;public class Main {
+package no.arnemunthekaas;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
